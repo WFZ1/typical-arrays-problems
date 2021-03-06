bdd2634ba7214d433a6544c513e2d45275056244
WFZ1 <pavelskumenko@gmail.com>
2021-03-06 10:46:36 +0200

    feat: realize the code which pass tests

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
+exports.min = array => (array && array.length) ? Math.min(...array) : 0;
+exports.max = array => (array && array.length) ? Math.max(...array) : 0;
+exports.avg = array => {
+  if (!array || !array.length) return 0;
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  array.forEach(el => sum += el);
+  return sum / array.length;
 }
